cc552a58294236657a4afa1373006ff0b576f5a8
Maximum Fredrick <102311122+MaxEfuru@users.noreply.github.com>
2023-04-28 22:58:42 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
-# code creat directory structures install nginx
 
-if ! which nginx > /dev/null; then
-	sudo apt-get update
-	sudo apt-get -y install nginx
+# Install Nginx if it is not already installed
+if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get update
+  sudo apt-get -y install nginx
 fi
 
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/
+# Create the necessary folders if they don't exist
+sudo mkdir -p /data/web_static/{releases,test,shared}
 
+# Create a fake HTML file to test Nginx configuration
+echo "<html>
+  <head>
+    <title>Testing Nginx</title>
+  </head>
+  <body>
+    <p>Testing Nginx configuration</p>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo echo "<html><head><title> Test page</title></head><body><p> This is a test page. </p></body></html>" | sudo tee /data/web_static/releases/test/index.html
+# Create symbolic link
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
+# Give ownership of /data/ folder to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
+# Update Nginx configuration
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
-service nginx restart
+# Restart Nginx
+sudo service nginx restart
