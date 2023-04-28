ebc02114a9cefc18243a465b9ebeeeb64a45873c
Maximum Fredrick <102311122+MaxEfuru@users.noreply.github.com>
2023-04-28 23:10:44 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
 # Install Nginx if it is not already installed
+
 if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   sudo apt-get update
